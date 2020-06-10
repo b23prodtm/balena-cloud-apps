f0cc08b94940644f0b8a300d0eeb57973822f287
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-06-10 22:40:07 +0200

    1.0.10

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,7 +3,7 @@ chmod -R +w .
 npm init --yes
 npm link balena-cloud
 cp node_modules/balena-cloud/test/build/*.env .
-read -r -a BALENA_PROJECTS < <(find . -name "Dockerfile*" | awk -F"Dockerfile" '{ print $1 }' | uniq | xargs)
+read -r -a BALENA_PROJECTS < <(find . -name "Dockerfile*" | awk -F"/Dockerfile" '{ print $1 }' | uniq | xargs)
 printf "Found %s BALENA_PROJECTS(" "${#BALENA_PROJECTS}"
 printf "%s " "${BALENA_PROJECTS[@]}"
 printf " )\n"
