b8d7d4c3f92714ad7c53123c505325f137ecb802
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-06-15 22:21:31 +0200

    Fixes: Primary DIND CircleCI Image

--- test/build-test.sh
+++ test/build-test.sh
@@ -8,36 +8,47 @@ testd="$(cd "$(dirname "${BASH_SOURCE[0]}")/build" && pwd)"
 LOG=$(new_log "/tmp")
 [ "$DEBUG" ] && LOG=$(new_log "/dev" "/stderr")
 function test_deploy() {
+  # x86_64
   args=("3" --nobuild --exit)
   # shellcheck disable=SC1090
   . "$vendord/balena_deploy.sh" "$testd" "${args[@]}" >> "$LOG"
   grep -q "intel-nuc" < "$testd/docker-compose.x86_64" || true
 }
 function test_deploy_2() {
+  # aarch64
   args=("2" --nobuild --exit)
   # shellcheck disable=SC1090
   . "$vendord/balena_deploy.sh" "$testd" "${args[@]}" >> "$LOG"
   grep -q "generic-aarch64" < "$testd/docker-compose.aarch64" || true
 }
 function test_deploy_3() {
+  # armhf
   args=("1" --nobuild --exit)
   # shellcheck disable=SC1090
   . "$vendord/balena_deploy.sh" "$testd" "${args[@]}" >> "$LOG"
   grep -q "raspberrypi3" < "$testd/docker-compose.armhf" || true
 }
-test_deploy
-results=("$?")
-test_deploy_2
-results+=("$?")
-test_deploy_3
-results+=("$?")
-function test_docker() {
+function test_docker_3() {
   args=("${testd}/submodule" -m . "betothreeprod/raspberrypi3" "$DKR_ARCH")
   # shellcheck disable=SC1090
   . "$vendord/docker_build.sh" "${args[@]}" >> "$LOG"
   docker image ls -q "${args[3]}*"
 }
+function test_docker() {
+  args=("${testd}/deployment/images/dind-php7" -m . "betothreeprod/dind-php7" "$DKR_ARCH")
+  # shellcheck disable=SC1090
+  . "$vendord/docker_build.sh" "${args[@]}" >> "$LOG"
+  docker image ls -q "${args[3]}*"
+}
+test_deploy
+results=("$?")
 test_docker
+results+=("$?")
+test_deploy_2
+results+=("$?")
+test_deploy_3
+results+=("$?")
+test_docker_3
 results+=("$?")
 [ "$chkSet" = 'x' ] && unset DEBIAN_FRONTEND || DEBIAN_FRONTEND=${chkSet:2}
 check_log "$LOG"
