bdc4364698ab9bd779bd6aba253b6e7ea3104e3d
BNOPOD <b23prodtm@users.sourceforge.net>
2022-10-31 23:36:01 +0100

    git retag loop

--- vendor/cni/git-retag.sh
+++ vendor/cni/git-retag.sh
@@ -2,8 +2,8 @@
 if [ -z "$1" ]; then echo "Usage: $0 <v0.0.0>"; exit; fi
 git tag -d "$1"
 git tag "$1"
-git push --tags || read -rp "git push --tags -f(orce) ?" force
-while [ -n "$force" ]; do
+git push --tags || while true; do
+  read -rp "git push --tags -f(orce) ? [y/n]" force
   case $force in
     Y|y) git push --tags -f; break;;
     N|n) break;;
