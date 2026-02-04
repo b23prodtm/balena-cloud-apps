9bc7c955b6c1fb141c47888b5ab734b03d102c76
B R T <b23prodtm@users.sourceforge.net>
2026-02-04 09:00:51 +0100

    armhf pushed submodule

--- vendor/cni/git_fix_issue.sh
+++ vendor/cni/git_fix_issue.sh
@@ -6,15 +6,17 @@ else
   . "$(command -v init_functions)" "${BASH_SOURCE[0]:-0}"
   [ "${DEBUG:-0}" != 0 ] && log_daemon_msg "passed args $*"
 
-  LOG=${LOG:-"$(new_log "." "$(basename "$project_root").log")"}
   printf "New issue fixture %s ...\n" "$1"
+  LOG=${LOG:-"$(new_log)"}
+
   sleep 1
-  if [ "$(git branch fix/issue-"$1" >> "$LOG")" ]; then
+
+  if git branch fix/issue-"$1" >> "$LOG"; then
      printf "Branch created\n"
   fi
   printf "Jump on branch %s...\n" "fix/issue-$1"
   sleep 1
-  if [ "$(git checkout fix/issue-"$1" >> "$LOG")" ]; then
+  if git checkout fix/issue-"$1" >> "$LOG"; then
      printf "Now fixing, add your files...\n"
   fi
 fi
--- vendor/cni/git_fix_issue_close.sh
+++ vendor/cni/git_fix_issue_close.sh
@@ -6,21 +6,21 @@ else
   . "$(command -v init_functions)" "${BASH_SOURCE[0]:-0}"
   [ "${DEBUG:-0}" != 0 ] && log_daemon_msg "passed args $*"
 
-  LOG=${LOG:-"$(new_log "." "$(basename "$project_root").log")"}
+  LOG=${LOG:-"$(new_log)"}
   
   printf "Close fixture %s ...\n" "$1"
   sleep 1
-  if [ "$(git checkout "$2" >> "$LOG")" ]; then
+  if git checkout "$2" >> "$LOG"; then
     printf "Switched to %s\n" "$2"
   fi
   printf "Delete branch %s\n" "fix/issue-$1"
   sleep 1
-  if [ "$(git branch -D fix/issue-"$1" >> "$LOG")" ]; then
+  if git branch -D fix/issue-"$1" >> "$LOG"; then
     printf "Branch was deleted !\n"
   fi
   printf "Pulling recent changes...\n"
   sleep 1
-  if [ "$(git pull >> "$LOG")" ]; then
+  if git pull >> "$LOG"; then
     printf "Done.\n"
   fi
 fi
